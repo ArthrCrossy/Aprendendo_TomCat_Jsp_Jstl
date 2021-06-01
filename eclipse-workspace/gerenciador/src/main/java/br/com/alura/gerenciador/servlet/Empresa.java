b8c63180b6cb02b38e6ad7ff7f5aaaee8a2ce950
ArthurCrossy <arthurcrossy@gmail.com>
2021-06-01 18:55:09 -0300

    atualiza

---
+++ eclipse-workspace/gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Empresa.java
@@ -0,0 +1,24 @@
+package br.com.alura.gerenciador.servlet;
+
+public class Empresa {
+	
+	private Integer id;
+	private String nome;
+	
+	public int getID() {
+		return id;
+	}
+	
+	public void setID(Integer id) {
+		this.id=id; 
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
